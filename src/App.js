2160d5231ab589c95b3c3158b26377e68e21d851
esroleo <esroleo@gmail.com>
2021-04-22 16:50:12 -0400

    front end set up tests with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import Nav from './components/Nav';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Toast from 'react-bootstrap/Toast';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Form from 'react-bootstrap/NavDropdown';
+import FormControl from 'react-bootstrap/NavDropdown';
+
+/* <Container>
+  <Row className="justify-content-md-between">
+    <Col sm={8}>Esteban</Col>
+    <Col sm={4}>
+      <ul>
+        <li>About Me</li>
+        <li>Contact</li>
+      </ul>
+    </Col>
+
+  </Row>
+// </Container> 
+*/
+
+
 
 function App() {
+  
+  const [show, toggleShow] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    
+    <Navbar bg="primary" variant="dark" expand="lg">
+      <Navbar.Brand href="#home">Esteban Romero</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+
+          <Nav.Link href="#About Me">Home</Nav.Link>
+          <Nav.Link href="#Portfolio">Portofilio</Nav.Link>
+          <Nav.Link href="#Contact">Contact</Nav.Link>
+          <Nav.Link href="#Resume">Resume</Nav.Link>
+
+        </Nav>
+      </Navbar.Collapse>
+</Navbar>
+
+      
+
+
+);
 }
 
 export default App;
