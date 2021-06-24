92c0a6b709a29a3280fda7d1fb3a589706f23982
esroleo <esroleo@gmail.com>
2021-06-24 09:57:29 -0400

    projects deployed url added

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -25,50 +25,65 @@ function Portfolio() {
             name: 'challeng-1-refactor-code-smaller',
             technology: 'HTML/CSS',
             description: 'Challenge to refactor code with best practices',
-            link: "https://esroleo.github.io/html-css-git-challenge1-code-refactor/"
+            deployedLink: "https://esroleo.github.io/html-css-git-challenge1-code-refactor/",
+            gitHubRepo: "https://github.com/esroleo/html-css-git-challenge1-code-refactor"
         },
         {
             title: 'Responsive Application',
             name: 'run-buddy-project',
             technology: 'HTML/CSS',
             description: 'Turn page into a responsive design with cool features',
-            link: "https://esroleo.github.io/run-buddy/"
+            deployedLink: "https://group5-project3-uot-duolingo.herokuapp.com/",
+            gitHubRepo: "https://github.com/esroleo/run-buddy"
         },
         {
             title: 'Password Generator',
             name: 'password-generator',
             technology: 'CSS/Javascript',
             description: 'Dynamic javascript project to render page as SPA',
-            link: "https://esroleo.github.io/javascript-challenge-password-generator/"
+            deployedLink: "https://esroleo.github.io/javascript-challenge-password-generator/",
+            gitHubRepo: "https://github.com/esroleo/javascript-challenge-password-generator"
         },
         {
             title: 'Web API Code Quiz',
             name: 'web-api-code-quiz',
             technology: 'Javascript/CSS/LocalStorage',
             description: 'Local storage used to store objects using json',
-            link: "https://esroleo.github.io/web-api-challenge-code-quiz/"
+            deployedLink: "https://esroleo.github.io/web-api-challenge-code-quiz/",
+            gitHubRepo: "https://github.com/esroleo/web-api-challenge-code-quiz"
         },
         {
             title: 'Work Day Scheduler',
             name: 'work-day-scheduler',
             technology: 'Javascript/Bootstrap',
             description: 'Dynamic application using jquery',
-            link: "https://esroleo.github.io/work-day-scheduler/"
+            deployedLink: "https://esroleo.github.io/work-day-scheduler/",
+            gitHubRepo: "https://github.com/esroleo/work-day-scheduler"
         },
         {
             title: 'Project01 Grouo07',
             name: 'project1-group07',
             technology: 'HTML/CSS/Javascript',
             description: `Group project 1 uses external api's with fetch to support COVID patients`,
-            link: "https://uot-project1-group07.github.io/project1-group07/"
+            deployedLink: "https://uot-project1-group07.github.io/project1-group07/",
+            gitHubRepo: "https://github.com/Uot-Project1-Group07/project1-group07"
         },
         {
             title: 'Project02 Group05',
             name: 'project2-group05',
             technology: 'HTML/CSS/Javascript/Sequelize/API',
             description: `Group project 2 ueses internal api's routes to talk to sequelize to fetch our data for our inventory application`,
-            link: "https://github.com/Bootcamp-Group5"
-        }
+            deployedLink: "https://simply-unused.herokuapp.com/",
+            gitHubRepo: "https://github.com/Bootcamp-Group5/simply-unused"
+        },
+        {
+            title: 'Project03 Group05',
+            name: 'project03-group05-mern',
+            technology: 'MERN Stack',
+            description: `Group project 2 ueses internal api's routes to talk to sequelize to fetch our data for our inventory application`,
+            deployedLink: "https://group5-project3-uot-duolingo.herokuapp.com/",
+            gitHubRepo: "https://github.com/Uot-Project3-Group05/uot-project3-group5"
+        },
       ];
 
 
@@ -111,10 +126,21 @@ function Portfolio() {
                         text="light" 
                         bg="primary" 
                         variant="primary"
-                        href={image.link}
+                        href={image.gitHubRepo}
+                        block
                         target="_blank">
-                            Go to Project
-                            </Button>
+                            Go to Project GitHub
+                        </Button>
+                        <Button 
+                        className="mb-2 text-center" 
+                        text="light" 
+                        bg="primary" 
+                        variant="primary"
+                        href={image.deployedLink}
+                        block
+                        target="_blank">
+                            Go to Deployed Project
+                        </Button>
                     </Card.Body>
                     </Card>
 
