e0527d18abe84d09dbe29b17dd7877e4080b72b1
esroleo <esroleo@gmail.com>
2021-07-08 13:32:10 -0400

    form control changes and email added

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -9,9 +9,9 @@ import { Container } from 'react-bootstrap';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 
-
-
-
+//mailer
+// npm install emailjs-com --save
+import emailjs from "emailjs-com";
 
 
 
@@ -19,43 +19,50 @@ function ContactForm() {
 
     // hook that will manage the form data as a controlled compoennt
     //const [formState, setFormState] = useState();
-    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const [formState, setFormState] = useState({ name: '', email: '', subject: '', message: '' });
 
     // hook to validate error message of the form
     const [errorMessage, setErrorMessage] = useState('');
 
     // Show Modal after submitting form
     const [show, setShow] = useState(false);
-
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
 
 
     // deconstruct formState
-    const { name, email, message } = formState;
+    const { name, email, subject, message } = formState;
 
     const handleSubmit = (e) => {
         e.preventDefault();
         
         if (!errorMessage) {
           setFormState({ [e.target.name]: e.target.value });
-          console.log('Form', formState);
+          //console.log('Form', formState);
+          sendEmail(e)
           handleShow()
+          console.log(e.target.message)
+          setFormState({ name: '', email: '', subject: '', message: '' });
+          e.target.reset()
           
-         } 
+         }
       };
 
+
+    // Manage form validation
     const handleChange = (e) => {
         if (e.target.name === 'email') {
             const isValid = validateEmail(e.target.value);
+            //xor logic
             if (!isValid) {
             setErrorMessage('Your email is invalid.');
-            } else {
+            } else { // No error
             setErrorMessage('');
             }
         } else {
             if (!e.target.value.length) {
+                // .name is the attribute of the html tag
             setErrorMessage(`${e.target.name} is required.`);
             } else {
                setErrorMessage('');
@@ -64,9 +71,24 @@ function ContactForm() {
         //console.log('errorMessage', errorMessage); 
         // if form data has passed then assigned correct form state
         if (!errorMessage) {
+            // ...formState is to use as is in terms of the variables assigned
+            // if there is no error. Change the variable stored to its new value.
             setFormState({ ...formState, [e.target.name]: e.target.value });
             }
     };
+
+    // send email function
+    function sendEmail(e) {
+        e.preventDefault();
+    
+        emailjs.sendForm('gmail', 'template_aj3gfi7', e.target, 'user_zs4TMIym9RgkXAzkAAKEz')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          //e.target.reset()
+      }
 
   
     return (
@@ -92,7 +114,16 @@ function ContactForm() {
                 </Form.Text>
             </Form.Group>
 
-            <Form.Group controlId="exampleForm.ControlTextarea1">
+            <Form.Group controlId="formEmailSubject">
+                <Form.Label>Email Subject</Form.Label>
+                <Form.Control name="subject" defaultValue={subject} onBlur={handleChange} size="lg" type="text" placeholder="Enter subject" />
+                <Form.Text className="text-muted">
+                
+                </Form.Text>
+            </Form.Group>
+
+
+            <Form.Group controlId="formEmailMessage">
                 <Form.Label>Leave me a message</Form.Label>
                 <Form.Control name="message" defaultValue={message} onBlur={handleChange} as="textarea" rows={3} />
             </Form.Group>
